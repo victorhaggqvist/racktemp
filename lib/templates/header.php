ac2a1c97bfd3e97ceda70f169dd7fa6041425bbd
Victor Häggqvist <victor@snilius.com>
2013-11-07 22:26:19 +0100

    added metatag for mobile-web-app-capable

--- lib/templates/header.php
+++ lib/templates/header.php
@@ -3,6 +3,7 @@
 <title></title>
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="mobile-web-app-capable" content="yes">
 
 <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
