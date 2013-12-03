0f174ff5b9ed6b73837e41afa64d214680c4a753
Victor Häggqvist <victor@snilius.com>
2013-12-03 16:37:32 +0100

    unnessecary scripts removed

--- lib/templates/footer.php
+++ lib/templates/footer.php
@@ -1,8 +1,7 @@
 <footer>Copyright &copy; 2013 Snilus.com <div class="pull-right">Licensed under GPL v2</div></footer>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="lib/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-<script src="lib/js/plugins.js"></script>
-<script src="lib/js/main.js"></script>
+
 
 <?php 
 if($s->getValue('use-cdn')==1){
