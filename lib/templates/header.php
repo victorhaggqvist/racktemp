20fe9c17461fbf6979b853d865e2553359fbd644
Victor Häggqvist <victor@snilius.com>
2013-12-12 00:05:53 +0100

    .

--- lib/templates/header.php
+++ lib/templates/header.php
@@ -1,9 +1,11 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title></title>
+<title>RackTemp</title>
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="mobile-web-app-capable" content="yes">
+<link rel="icon" href="favicon.ico" />
+<link rel="apple-touch-icon-precomposed" href="/img/apple-touch-icon-precomposed.png" type="image/png">
 
 <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
