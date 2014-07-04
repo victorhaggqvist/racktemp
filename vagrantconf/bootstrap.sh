21d2eb9684506247ed172a2fc4d88782604fe755
Victor Häggqvist <victor@snilius.com>
2014-07-05 01:57:20 +0200

    set timezone to actual

--- vagrantconf/bootstrap.sh
+++ vagrantconf/bootstrap.sh
@@ -4,7 +4,7 @@ sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_aga
 
 sudo apt-get update
 sudo apt-get -y updgrade
-sudo apt-get -y install nginx php5 php5-fpm php5-mysql php5-gd vim mysql-server-5.5 php5-curl php5-cli screen
+sudo apt-get -y install nginx php5 php5-fpm php5-mysql php5-gd vim mysql-server-5.5 php5-curl php5-cli screen unzip
 
 # bind mysql to all
 cat /etc/mysql/my.cnf | sed 's/bind-address/#bind-address/' > mymod.cnf
@@ -21,8 +21,19 @@ mysql -uroot -proot -e "source /vagrant/racktemp.sql"
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /vagrant/vagrantconf/racktemp.conf /etc/nginx/sites-enabled/racktemp.conf
 
+# set timezone
+echo "Config Timezone"
+wget -q -O tzupdate.zip https://github.com/victorhaggqvist/tzupdate/archive/master.zip
+unzip -q tzupdate.zip
+export TZ=$(./tzupdate-master/tzupdate -p)
+
 # php setup
 sudo echo "display_errors = On" | sudo tee -a /etc/php5/fpm/php.ini
+sudo echo "date.timezone = '${TZ}'" | sudo tee -a /etc/php5/fpm/php.ini
+
+# set system timezone
+sudo echo ${TZ} | sudo tee -a /etc/timezone
+sudo cp /usr/share/zoneinfo/${TZ} /etc/localtime
 
 sudo service nginx restart
 sudo service mysql restart
