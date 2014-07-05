7129fae394e03060ab0cb6d73cac03bd4159f39f
Victor Häggqvist <victor@snilius.com>
2014-07-05 05:07:50 +0200

    works

--- app/lib/classes/Snilius/RackTemp/GraphApi.php
+++ app/lib/classes/Snilius/RackTemp/GraphApi.php
@@ -40,7 +40,8 @@ class GraphApi {
 
       // timestamps for the x axis in c3 chart
       if ($i == 0) {
-        $response[$i][] = 'x';
+        $response[$i] = array();
+        $response[$i][] = "x";
         foreach ($stat as $s)
           $response[$i][] = $s['timestamp'];
       }
@@ -48,19 +49,40 @@ class GraphApi {
       // show only sensors with recent stats
       if (count($stat)>1) {
         $activeSensors++;
+        $response[$i+1] = array();
         $response[$i+1][] = $sensors[$i]->name;
         foreach ($stat as $s)
           $response[$i+1][] = $s['temp'];
       }
     }
 
-    $ret = '';
-    $cvsCols = $activeSensors + 1;
+    // $ret = '';
+    // $cvsCols = $activeSensors + 1;
+    // json_encode($response);
     // var_dump($cvsCols);
     // var_dump($response);
-    for ($i=0; $i < $cvsCols; $i++) {
-      for ($j=0; $j < count($response[0]); $j++) {
-        $ret .= $response[$j][$i].($j == $cvsCols?'':',');
+    // $ret .= implode(',',$response[0]);
+    // for ($i=0; $i < $cvsCols; $i++) {
+    //   for ($j=0; $j < count($response[0]); $j++) {
+    //     $ret .= $response[$j][$i].($j == $cvsCols?'':',');
+    //   }
+    //   $ret.="\n";
+    // }
+    // $ret = "[\n";
+
+    // $i = 0;
+    // foreach ($response as $key => $value) {
+    //   $ret .= "['".$key."'";
+    //   foreach ($value as $v) {
+    //     $ret .= ($key == 'x')? ",'".$v."'":','.$v;
+    //   }
+    //   $ret .= (++$i<count($response))?"],\n":"]\n";
+    // }
+
+    // $ret .= ']';
+
+    return json_encode($response);
+  }
       }
       $ret.="\n";
     }
