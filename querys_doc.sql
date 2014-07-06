c278dcd218a5bf87fab277278fcefca94223d34e
Victor Häggqvist <victor@snilius.com>
2014-07-06 02:01:19 +0200

    some docs in non obvious queries

---
+++ querys_doc.sql
@@ -0,0 +1,12 @@
+# week stats
+SELECT temp, timestamp FROM (   # wrapper to strip of timekey
+SELECT
+  ROUND(AVG(temp)) AS temp,
+  DATE_ADD(
+    DATE_FORMAT(timestamp, "%Y-%m-%d %H:00:00"),
+    INTERVAL IF(HOUR(timestamp)%2<1,0,1) HOUR  # push all timestamps to even to make them preditable, IF is true if statement <> 0 thefor MOD<1
+  ) AS timestamp,
+  ROUND(UNIX_TIMESTAMP(timestamp) / (120 * 60)) AS timekey
+FROM sensor_mysen
+WHERE timestamp >= NOW() - INTERVAL 1 WEEK
+GROUP BY timekey) as musthav;   # must have alias apperently
