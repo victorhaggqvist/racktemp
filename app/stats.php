6276f381747364de10c4d1f59bd979abf10c9aed
Victor Häggqvist <victor@snilius.com>
2014-07-06 04:52:22 +0200

    week stats

--- app/stats.php
+++ app/stats.php
@@ -9,14 +9,16 @@ require_once 'lib/head.inc' ;
   <body>
     <?php require_once TEMPLATES_PATH.'/menu.php'; ?>
     <div class="container wrapper">
-      <h3>Today</h3>
+
       <div class="row">
         <div class="col-md-6">
+          <h3>Today</h3>
           <div id="today"></div>
         </div>
         <div class="col-md-6">
+          <h3>Week</h3>
           <!-- <div id="today2" style="width: 500px; height:200px;"></div> -->
-          <div id="today2"></div>
+          <div id="week"></div>
         </div>
       </div>
 
@@ -25,34 +27,13 @@ require_once 'lib/head.inc' ;
   </body>
 
   <?php
-  $api = new \Snilius\RackTemp\Api();
+  $api = new \Snilius\RackTemp\Api\Api();
    ?>
 
   <script>
-  RackTemp.setApiAuth(<?php echo $api->getWebKey(); ?>); // jshint ignore:line
-  function fetchData(url, callback){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== 4 || xhr.status !== 200){ return; }
-      callback(xhr.responseText);
-    };
-    xhr.send();
-  }
-
-  var chart = c3.generate(RackTemp.chart.today);
-
-  // fetch today
-  fetchData(makeApiUrl('graph/span/day'), function(resp){
-    var chartData = JSON.parse(resp);
-    if (RackTemp.isChartEmpty(chartData)) {
-      document.getElementById('today').innerHTML = 'No chart data';
-    }else{
-      chart.load({
-        columns: chartData
-      });
-    }
-  });
+  RackTemp.setApiInfo(<?php echo $api->getWebKey(); ?>); // jshint ignore:line
+  RackTemp.createChartToday();
+  RackTemp.createChartWeek();
 
   </script>
 </html>
