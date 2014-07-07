dd0fc462b5a8fae7c785ade30098616b963d5764
Victor Häggqvist <victor@snilius.com>
2014-07-07 23:33:36 +0200

    more settings

--- app/settings.php
+++ app/settings.php
@@ -16,6 +16,7 @@ require_once 'lib/head.inc';
         <li><a href="#logging" data-toggle="tab">Logging</a></li>
         <li><a href="#general" data-toggle="tab">General</a></li>
         <li><a href="#api" data-toggle="tab">API</a></li>
+        <li><a href="#notifications" data-toggle="tab">Notifications</a></li>
       </ul>
 
       <div class="tab-content">
@@ -34,6 +35,10 @@ require_once 'lib/head.inc';
         <div class="tab-pane" id="api">
           <?php require_once 'settings-api.php'; ?>
         </div>
+
+        <div class="tab-pane" id="notifications">
+          <?php require_once 'settings-notifications.php'; ?>
+        </div>
       </div><!-- /.tab-content -->
 
       <?php require_once TEMPLATES_PATH.'/footer.php'; ?>
@@ -41,6 +46,7 @@ require_once 'lib/head.inc';
 
     <script>
     RackTemp.loadToTab();
+    RackTemp.smtpSettingsSetup();
     $('.dropData').click(function(){
       console.log($(this));
       var sensor = $(this).attr('data-sensor');
