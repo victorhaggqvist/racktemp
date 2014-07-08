b07b90b394837ca6eb3048315a5680dfce27393c
Victor Häggqvist <victor@snilius.com>
2014-07-08 21:10:53 +0200

    py slice

--- php/Sensor/SensorTools.php
+++ php/Sensor/SensorTools.php
@@ -32,5 +32,31 @@ abstract class SensorTools {
       $ret=$temp;
     return $ret;
   }
+
+  /**
+   * Implementation of parts of pythons slice behavior. Well it covers the part I want.
+   * From http://se2.php.net/manual/en/function.substr.php#103143
+   * @param  string $input
+   * @param  string $slice
+   * @return string
+   */
+  private function py_slice($input, $slice) {
+      $arg = explode(':', $slice);
+      $start = intval($arg[0]);
+      if ($start < 0) {
+          $start += strlen($input);
+      }
+      if (count($arg) === 1) {
+          return substr($input, $start, 1);
+      }
+      if (trim($arg[1]) === '') {
+          return substr($input, $start);
+      }
+      $end = intval($arg[1]);
+      if ($end < 0) {
+          $end += strlen($input);
+      }
+      return substr($input, $start, $end - $start);
+  }
 }
 ?>
