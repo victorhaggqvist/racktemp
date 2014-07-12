3cc803bb49d767c4b4b8c6b6027ef2333d392906
Victor Häggqvist <victor@snilius.com>
2014-07-12 14:34:45 +0200

    fix ticks

--- app/lib/analytics.php
+++ app/lib/analytics.php
@@ -6,6 +6,6 @@
 
   ga('create', 'UA-46767520-3', '<?php echo $_SERVER["SERVER_NAME"]; ?>');
   ga('send', 'pageview');
-  <?php echo isset($_COOKIE["PHPSESSID"])?"ga(‘set’, ‘&uid’, '".$_COOKIE["PHPSESSID"]."');":''; ?>
+  <?php echo isset($_COOKIE["PHPSESSID"])?"ga('set', '&uid', '".$_COOKIE["PHPSESSID"]."');":''; ?>
 
 </script>
