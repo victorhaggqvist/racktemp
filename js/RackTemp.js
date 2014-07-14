2aee70fd5288d269e38ae7fdfd706703734ea7cd
Victor Häggqvist <victor@snilius.com>
2014-07-14 03:03:36 +0200

    grunt

--- js/RackTemp.js
+++ js/RackTemp.js
@@ -165,8 +165,7 @@ var RackTemp = (function (){
   var _timestamp, _token, _host;
 
   var _makeApiUrl = function (apiPath){
-    // TODO: Make protocol dynamic, maby send it with the vars from setApiInfo
-    // using PHP check on HTTP_REFERER for https
+    // TODO: check on HTTP_REFERER PHP
     return '//'+_timestamp+':'+_token+'@'+_host+'/api/'+apiPath;
   };
 
