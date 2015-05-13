dece1210c146183e29e33d1aaf9d1ee6642d49ea
Victor Häggqvist <victor@snilius.com>
2015-05-13 03:12:20 +0200

    homescreen manifest

--- app/lib/templates/header.php
+++ app/lib/templates/header.php
@@ -4,6 +4,7 @@
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="mobile-web-app-capable" content="yes">
+<link rel="manifest" href="manifest.json">
 <link rel="icon" href="favicon.ico" />
 <link rel="apple-touch-icon-precomposed" href="/img/apple-touch-icon-precomposed.png" type="image/png">
 
