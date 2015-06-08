00a354b6b9878012fde58c8f48c3cd8506076527
Victor Häggqvist <victor@snilius.com>
2015-06-09 00:18:02 +0200

    cron runner

--- src/AppBundle/Console/CronCommand.php
+++ src/AppBundle/Console/CronCommand.php
@@ -24,6 +24,7 @@ class CronCommand extends ContainerAwareCommand {
 
     protected function execute(InputInterface $input, OutputInterface $output) {
         $output->writeln('cronie');
+        $logger = $this->getContainer()->get('logger');
 
         $sensorController = $this->getContainer()->get('app.sensor.sensor_controller');
         $sensors = $sensorController->getAttachedSensors();
@@ -31,6 +32,7 @@ class CronCommand extends ContainerAwareCommand {
         $body = [];
         foreach ($sensors as $s) {
             $body[$s] = $sensorController->readSensorByUid($s);
+            $logger->info(sprintf('cron for %s', $s));
         }
 
         $client = new Client();
@@ -40,6 +42,7 @@ class CronCommand extends ContainerAwareCommand {
 
         if (!preg_match('/http/', $master)) {
             $output->writeln("there is no protocol in 'master_host'");
+            $logger->warning("there is no protocol in 'master_host', aborting");
             return;
         }
 
@@ -49,8 +52,13 @@ class CronCommand extends ContainerAwareCommand {
         $output->writeln($jsonbody);
 
         $req = $client->createRequest('POST', sprintf('%s/api/record?token=%s&timestamp=%s', $master, $token, $timestamp), null, $jsonbody);
-//        $client->send($req);
+        $resp = $client->send($req);
 
+        if ($resp->getStatusCode() == 201) {
+            $logger->info('data sent');
+        } else {
+            $logger->warning(sprintf('slave -> master failed: (%s) %s', $resp->getStatusCode(), $resp->getBody()));
+        }
     }
 
 
